89cd9450594b87644374e0e8c3ca203615d9b9aa
Julieth Campos <102711785+JuliethCP@users.noreply.github.com>
2022-08-15 21:59:18 -0600

    Update ejercicio1.go

--- Práctica Imperativa 1/ejercicio1.go
+++ Práctica Imperativa 1/ejercicio1.go
@@ -16,7 +16,6 @@ import (
 
 func contadorTexto(text string) {
 
-	fmt.Println("--------------------------------------------------\n", text, "--------------------------------------------------")
 	//contar los caracteres
 	tex := strings.Fields(text)                       //divide la cadena de texto en palabras "hola","me", crea un slice de substrings of str
 	union := strings.Join(tex, "")                    //une los subStrings
@@ -26,22 +25,34 @@ func contadorTexto(text string) {
 	fmt.Println("Numero de palabras: ", len(tex)) //cuenta los datos del slice, en este caso cuenta cada substring(palabra)
 
 	//contar las líneas
-	n := strings.Count(text, "\n")
-	fmt.Println("Cantidad de lineas: ", n)
+	n := strings.Split(text, "\n")
+	fmt.Println("Cantidad de lineas: ", len(n))
+
 }
 
+// split
 func main() {
 
-	texto := "La Tecnología se define como el conjunto de conocimientos y técnicas que,\n" +
-		"aplicados de forma lógica y ordenada, permiten al ser humano modificar su entorno \n" +
-		"material o virtual para satisfacer sus necesidades, esto es, un proceso combinado de \n" +
-		"pensamiento y acción con la finalidad de crear soluciones útiles.\n"
-	/**
-	texto2 := "La Tecnología se define como el conjunto de conocimientos y técnicas que," +
-		"aplicados de forma lógica y ordenada, permiten al ser humano modificar su entorno" +
-		"material o virtual para satisfacer sus necesidades, esto es, un proceso combinado de" +
-		"pensamiento y acción con la finalidad de crear soluciones útiles."
-	*/
-	contadorTexto(texto)
+	texto :=
+		`La Tecnología se define como el conjunto de conocimientos y técnicas que,
+		aplicados de forma lógica y ordenada, permiten al ser humano modificar su entorno 
+		material o virtual para satisfacer sus necesidades, esto es, un proceso combinado de 
+		pensamiento y acción con la finalidad de crear soluciones útiles.`
 
+	texto2 := "La Tecnología se define como el conjunto de conocimientos y técnicas que,\n" +
+		"aplicados de forma lógica y ordenada, permiten al ser humano modificar su entorno\n" +
+		"material o virtual para satisfacer sus necesidades, esto es, un proceso combinado de\n" +
+		"pensamiento y acción con la finalidad de crear soluciones útiles.\n"
+	contadorTexto(texto)
+	fmt.Println("\n--------------------------------------------------\n", texto2, "--------------------------------------------------")
 }
+
+
+
+
+/**
+Consola:
+Numero de caracteres:  264
+Numero de palabras:  48
+Cantidad de lineas:  4
+*/
