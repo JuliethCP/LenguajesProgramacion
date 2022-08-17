44dc26963ada8d99f0bd70998b7907bb3b120a1b
Julieth Campos <102711785+JuliethCP@users.noreply.github.com>
2022-08-16 22:42:12 -0600

    Update ejercicio4.go

--- Práctica Imperativa 1/ejercicio4.go
+++ Práctica Imperativa 1/ejercicio4.go
@@ -1,5 +1,5 @@
 /*
-Fecha: 8/8/2020
+Fecha: 15/8/2020
 Hecho por: Julieth Campos
 Ejercicio 4:
 Escriba un programa que utilice una estructura y un arreglo/slice para almacenar en memoria
@@ -24,40 +24,109 @@ type calzado struct {
 	marca  string
 	precio int
 	talla  byte
-	id     byte
+	stock  byte
 }
 
-var listaZapatos []calzado
+type listaZapatos []calzado
 
-func crearZapato(m string, p int, t byte, i byte) calzado {
-	if t >= 34 && t <= 44 {
-		z := calzado{marca: m, precio: p, talla: t, id: i}
-		fmt.Println("Zapato creado con exito!!")
-		return z
-	} else {
-		fmt.Println("El numero de talla no es permitido")
-		y := calzado{}
-		return y
+var lZapatos listaZapatos
+
+func (l *listaZapatos) buscarZapato(marca string, talla byte) int { //el retorno es el índice del producto encontrado y -1 si no existe
+	var result = -1
+	var i int
+	for i = 0; i < len(*l); i++ {
+		if (*l)[i].marca == marca && (*l)[i].talla == talla {
+			result = i
+		}
 	}
-
+	return result
 }
 
-func (shoe *calzado) agregarZapato(z calzado) {
-	if shoe.marca == "" && shoe.precio == 0 && shoe.talla == 0 && shoe.id == 0 {
-		return
+func (l *listaZapatos) crearZapato(marca string, precio int, talla byte, stock byte) {
+	z := (*l).buscarZapato(marca, talla)
+	if z == -1 {
+		if talla >= 34 && talla <= 44 {
+			*l = append(*l, calzado{marca: marca, precio: precio, talla: talla, stock: stock})
+			println("El zapato ha sido creado exitosamente")
+		} else {
+			println("La talla especificada no coincide con las permitidas, favor intente de nuevo...")
+		}
+	} else {
+		if (*l)[z].marca == marca && (*l)[z].talla == talla {
+			s := (*l)[z].stock + stock
+			(*l)[z].stock = s
+		}
+	}
+}
+func remove(slice listaZapatos, s int) listaZapatos {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func (l *listaZapatos) venderZapato(marca string, talla byte, cant byte) {
+	var shoe = l.buscarZapato(marca, talla)
+	if shoe == -1 {
+		println("El zapato no existe, entonces no se puede vender")
+
+	} else if (*l)[shoe].stock >= cant {
+		(*l)[shoe].stock = (*l)[shoe].stock - cant
+		println("Zapato vendido con éxito")
+
+	} else if (*l)[shoe].stock == 0 {
+		println("No hay zapatos disponibles en el inventario")
+		remove(*l, shoe)
 
 	} else {
-		listaZapatos = append(listaZapatos, z)
+		println("La venta no puede ser ejecutada ya que no hay suficientes zapatos en el inventario para vender los solicitados")
 	}
-
 }
-
-// val inventario = listOf<Pair<Int,Int>>(
 func main() {
+	lZapatos.crearZapato("Nike", 25000, 35, 10)
+	lZapatos.crearZapato("Adidas", 35000, 37, 10)
+	lZapatos.crearZapato("DC", 35000, 40, 15)
+	lZapatos.crearZapato("DC", 40000, 35, 8)
+	lZapatos.crearZapato("Puma", 45000, 42, 10)
+	lZapatos.crearZapato("Puma", 25000, 36, 12)
+	lZapatos.crearZapato("Crocs", 15000, 38, 20)
+	lZapatos.crearZapato("Converse", 40000, 37, 10)
+	fmt.Println(lZapatos)
+	println("---------------AUMENTO DE STOCK------------------")
+	lZapatos.crearZapato("Nike", 25000, 35, 2)
+	lZapatos.crearZapato("DC", 45000, 40, 4)
+	lZapatos.crearZapato("Crocs", 45000, 38, 4)
 
-	z := crearZapato("Nike", 35000, 32, 1)
-	z.agregarZapato(z)
-	a := crearZapato("Adidas", 45000, 35, 1)
-	a.agregarZapato(a)
-	fmt.Println(listaZapatos)
+	fmt.Println(lZapatos)
+	println("-------------------VENTAS--------------------")
+	lZapatos.venderZapato("Nike", 35, 3)
+	lZapatos.venderZapato("Adidas", 37, 5)
+	lZapatos.venderZapato("DC", 40, 1)
+	lZapatos.venderZapato("Puma", 42, 3)
+	lZapatos.venderZapato("Puma", 36, 1)
+	lZapatos.venderZapato("Crocs", 38, 10)
+	fmt.Println(lZapatos)
 }
+
+/** --------------------------------------------------CONSOLA--------------------------------------------------
+El zapato ha sido creado exitosamente
+El zapato ha sido creado exitosamente
+El zapato ha sido creado exitosamente
+El zapato ha sido creado exitosamente
+El zapato ha sido creado exitosamente
+El zapato ha sido creado exitosamente
+El zapato ha sido creado exitosamente
+El zapato ha sido creado exitosamente
+[{Nike 25000 35 10} {Adidas 35000 37 10} {DC 35000 40 15} {DC 40000 35 8} {Puma 45000 42 10} {Puma 25000 36 12} {Crocs 15000 38 20} {Converse 40000 37 10}]
+---------------AUMENTO DE STOCK------------------
+[{Nike 25000 35 12} {Adidas 35000 37 10} {DC 35000 40 19} {DC 40000 35 8} {Puma 45000 42 10} {Puma 25000 36 12} {Crocs 15000 38 24} {Converse 40000 37 10}]
+-------------------VENTAS--------------------
+Zapato vendido con éxito
+Zapato vendido con éxito
+Zapato vendido con éxito
+Zapato vendido con éxito
+Zapato vendido con éxito
+Zapato vendido con éxito
+[{Nike 25000 35 9} {Adidas 35000 37 5} {DC 35000 40 18} {DC 40000 35 8} {Puma 45000 42 7} {Puma 25000 36 11} {Crocs 15000 38 14} {Converse 40000 37 10}]
+
+Process finished with the exit code 0
+
+*/
+
